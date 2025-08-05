52b5b57f30a8cc2dbae346603d26b1bd6d232cb8
Mikhail Russkikh <deazinew@mail.ru>
2025-08-05 23:17:09 +0500

    Prepare project for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,22 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  root: "src/views",
-  resolve: {
-    alias: {
-      "/css": resolve(__dirname, "src/css"),
-    },
-  },
+  root: "src/views", // корень для html — можно и "src"
   build: {
+    outDir: "../../dist", // папка для сборки (относительно root)
+    emptyOutDir: true, // очистить dist перед билдом
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/views/index.html"),
+        index: resolve(__dirname, "src/views/index.html"),
         about: resolve(__dirname, "src/views/about.html"),
         experience: resolve(__dirname, "src/views/experience.html"),
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "/assets": resolve(__dirname, "src/assets"),
+      "/css": resolve(__dirname, "src/css"),
     },
   },
 });
